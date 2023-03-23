152bdb27b5acd4cd1594c0bcf27698975cfc7109
unknown <kristofer.rosquist@gmail.com>
2023-03-23 22:04:58 +0100

    first commit in C#

---
+++ Advent2020_day5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Hello
+  {
+    static void Main(string[] args)
+    {
+
+        string[] lines = File.ReadAllLines("C:/Users/Kristofer Rosquist/Desktop/HoT/Training/Advent/2020_day5.txt");
+        
+
+        var IDs = new List<int>();
+        foreach (string li in lines){
+            int IDnew = 0;
+            for(int i = 0; i< 10; i++){
+                IDnew += (li[i] == 'B' || li[i] == 'R') ? (int)Math.Pow(2,7-i+2) : 0;
+            }
+         
+        IDs.Add(IDnew);
+        }
+
+        IDs.Sort();
+
+        //part1:
+        Console.WriteLine(IDs.Last());
+         
+       
+       //part 2:
+       for(int i=0; i< IDs.Count;i++){
+        if(IDs[i+1] != (IDs[i]+1)){
+            Console.WriteLine("part2: " + (IDs[i] + 1));
+            break;
+        }
+       }
+            
+    }
+  }
+}
---
+++ Advent2020_day6/Program.cs
@@ -0,0 +1,60 @@
+﻿//#define Solution1
+
+using System;
+
+
+  class MyProgram
+  {
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("C:/Users/Kristofer Rosquist/Desktop/HoT/Training/Advent/2020_day6.txt");
+
+        var charset = new HashSet<char>();
+        int count = 0;
+
+        #if Solution1
+        foreach(string line in lines){
+            if(line.Length <1){
+                count += charset.Count;
+                charset.Clear();
+            }else{
+                for(int i = 0; i<line.Length; i++){
+                    charset.Add(line[i]);
+                }
+                
+            }
+        }
+        #else
+
+        var chardict = new Dictionary<char,int>();
+
+        int NbrPassengers = 0;
+        foreach(string line in lines){
+            if(line.Length <1){
+                foreach(var el in chardict){
+                    if(el.Value == NbrPassengers){
+                        count++;
+                    }
+                }
+                NbrPassengers = 0;
+                chardict.Clear();
+            }else{
+                NbrPassengers++;
+                for(int i = 0; i<line.Length; i++){
+                    if(chardict.ContainsKey(line[i])){
+                        chardict[line[i]]++;
+                    }
+                    else{
+                        chardict.Add(line[i],1);
+                    }                    
+                }                
+            }
+        }
+
+        #endif   
+        
+
+       Console.WriteLine(count);
+    }
+  }
+
