9f7998a646ea0f7d5abd70aeebcc5d340b6991b9
unknown <kristofer.rosquist@gmail.com>
2023-03-27 15:24:58 +0200

    adding day 10 and 11

---
+++ Advent2020_day10/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+class DAY10
+{
+    static void Main(string[] args)
+    {
+        
+        string[] lines = File.ReadAllLines("C:/Users/Kristofer Rosquist/Desktop/HoT/Training/Advent/2020_day10.txt");
+
+        SortedSet<int> myset = new SortedSet<int>();
+
+        foreach(string line in lines){
+            myset.Add(Convert.ToInt32(line));
+        }
+
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+
+        myset.Add(0); //add outlet
+        myset.Add(myset.ElementAt(myset.Count()-1)+3); //adding my adapter
+
+        for(int i = 1; i< myset.Count(); i++){
+            int diff = myset.ElementAt(i)-myset.ElementAt(i-1);
+            count1 += diff == 1 ? 1 : 0;
+            count2 += diff == 2 ? 1 : 0;
+            count3 += diff == 3 ? 1 : 0;
+        }
+
+
+        for(int i = 1; i< myset.Count();i++){
+            int diff = myset.ElementAt(i)-myset.ElementAt(i-1);
+        }
+
+        Dictionary<int,long> mydict = new Dictionary<int, long>();
+
+        mydict[0] = 1; //1 way to get to first 0.
+        for(int i= 1; i< myset.Count(); i++){
+            int adapter = myset.ElementAt(i);
+            mydict[adapter] = mydict.GetValueOrDefault(adapter-3,0) +mydict.GetValueOrDefault(adapter-2,0) + mydict.GetValueOrDefault(adapter-1,0);
+
+        }
+        Console.WriteLine(count1 + " " + count3 + " mult:" + (count1*count3) + " for: " + myset.ElementAt(myset.Count()-1) + " possible ways: " + mydict[myset.ElementAt(myset.Count()-1)]);
+    
+    
+    
+        //using combination //math:
+        //no...
+        //(m^(n-1)) - ((m^(n-4)) * (m-1))
+        //Determine the number of elements in the set. Let's call this number "n".
+        //Count the number of "jumps" between adjacent values that are allowed. For example, 
+        //if the difference between adjacent values can only be 1 or 2, 
+        //there are two allowed jumps. Let's call this number "m".
+        //(m^(n-4)), represents the number of subsets that violate the condition (2 for 1 condition, 3 for 2)
+
+//ach term in the series is of the form
+// ((m^(n-k)) * (m-1)^k), where k is the number of pairs of adjacent elements with a difference of "d+k".
+
+/*
+F(0) = 1
+F(1) = m
+F(2) = m + (m-1)
+F(n) = F(n-1) + (m-1) * F(n-2) for n > 2
+m=2 and n=5.
+*/
+    
+    }
+
+
+
+}
---
+++ Advent2020_day11/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+class DAY11
+{
+
+    static int countel(ref char[,] map, char character, int i, int j, int maxRow, int maxColumn){
+        int count=0;
+
+        if(i < 0 || i > (maxRow-1) || j < 0 || j> (maxColumn-1)){
+            count = 0;
+        } else{
+            count += (map[i,j] == character) ? 1 : 0;
+        }
+        return count;
+
+    }
+
+
+    static int countAlladjacentSolution1(ref char[,] map, char character, int i, int j, int maxRow, int maxColumn){
+        int count = 0;
+        for(int dirx = -1; dirx<2; dirx++){
+            for(int diry= -1; diry<2; diry++){
+                if(!(dirx==0 & diry== 0)){
+                    count += countel(ref map,character,i+dirx,j+diry,maxRow,maxColumn);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    static int counteldirection(ref char[,] map, char character, int i, int j, int maxRow, int maxColumn, int dirX, int dirY){
+        int count=0;
+
+        if(i < 0 || i > (maxRow-1) || j < 0 || j> (maxColumn-1)){
+            count = 0;
+        } else{
+            if(map[i,j] == character){
+                return 1;
+            }
+            else if(map[i,j] == 'L'){   //if empty it cannot see occupied!
+                return 0;
+            }else{
+                count = counteldirection(ref map, character, i+dirX, j+dirY, maxRow, maxColumn, dirX, dirY);
+            }
+        }
+        return count;
+
+    }
+
+
+    static int countAlladjacentSolution2(ref char[,] map, char character, int i, int j, int maxRow, int maxColumn){
+        int count =  0;
+        
+        for(int dirx = -1; dirx<2; dirx++){
+            for(int diry= -1; diry<2; diry++){
+                if(!(dirx==0 & diry== 0)){
+                    count += counteldirection(ref map,character,i+dirx,j+diry,maxRow,maxColumn,dirx,diry);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    static void Main(string[] args)
+    {
+        
+        //new line etc. is not included so all strings are the same length!!
+        string[] lines = File.ReadAllLines("C:/Users/Kristofer Rosquist/Desktop/HoT/Training/Advent/2020_day11.txt");
+        
+        int NbrOfRows = lines.Count();
+        int NbrOFColumns = lines[0].Length;
+        
+        
+        char[,] map = new char[NbrOfRows,NbrOFColumns];
+        char[,] nextmap = new char[NbrOfRows,NbrOFColumns]; //if we do nextmap = map we get shallow copy and copye references!
+        
+       for(int i = 0; i < NbrOfRows; i++){
+
+            for(int j = 0; j< NbrOFColumns; j++){
+                map[i,j] = lines[i].ElementAt(j);
+                nextmap[i,j] = lines[i].ElementAt(j);
+            }
+        }
+
+        nextmap = map.Clone() as char[,];  //copy copies values, if we have pointers this is not deep enough if we want the objects.
+        
+        bool WasAChange = true;
+
+        while(WasAChange){
+            WasAChange = false;
+            for(int i = 0; i < NbrOfRows; i++){
+
+                for(int j = 0; j< NbrOFColumns; j++){
+                    
+                    switch(map[i,j]){
+
+                        case 'L': //available                            
+                            if(countAlladjacentSolution2(ref map,'#',i,j,NbrOfRows,NbrOFColumns) == 0){
+                                nextmap[i,j] = '#'; 
+                                WasAChange = true;
+                            } 
+                        break;
+
+                        case '#': //occopied
+                            if(countAlladjacentSolution2(ref map,'#',i,j,NbrOfRows,NbrOFColumns) >= 5){ //4 for solution 1, 5 for 2
+                                nextmap[i,j] = 'L'; 
+                                WasAChange = true;
+                            }       
+                        break;
+                    }                    
+                }
+            }
+             map = nextmap.Clone() as char[,];
+        }
+
+        long counter = 0;
+        for(int i = 0; i < NbrOfRows; i++){
+
+            for(int j = 0; j< NbrOFColumns; j++){
+                if(map[i,j] == '#'){                    
+                    counter++;
+                }
+                //Console.Write(map[i,j]);
+            }
+           // Console.Write('\n');
+        }
+
+        Console.WriteLine(counter);
+    }
+
+
+}
