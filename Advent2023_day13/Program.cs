76462b54d0fec43eb5971a9b1e8332753cbc5331
Kristofer Rosquist <kristofer.rosquist@gmail.com>
2024-01-08 18:12:26 +0100

    add 2 days

---
+++ Advent2023_day13/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+
+class myPattern{
+    public int startIndex = 0;
+    public int nbrOfRows = 0;
+    public int nbrOfColumns = 0;
+    public List<long> RowList = new();
+    public List<long> ColumnList = new();
+    public myPattern(int columns, int startind){
+        nbrOfColumns = columns;
+        startIndex = startind;
+    }
+}
+
+class DAY13
+{
+
+    static int findMirror(List<long> RowOrColumnList, int NbrOfRowOrColumn){
+        for(int i = 0; i < NbrOfRowOrColumn; i++ ){
+                int previousI = i - 1;
+                bool Found = false;
+                    if(previousI >= 0 && RowOrColumnList.ElementAt(i) == RowOrColumnList.ElementAt(previousI)){
+                        int nextI = i + 1;
+                        Found = true;
+                        while(previousI > 0 && nextI < NbrOfRowOrColumn){
+                            previousI--;
+                            Found = Found && (RowOrColumnList.ElementAt(nextI) == RowOrColumnList.ElementAt(previousI));
+                            nextI++;
+                            if(!Found){
+                                break;
+                            }
+                        }                        
+                    }
+                    if(Found){
+                        return i;
+                    }               
+            }
+
+            return 0;
+
+    }
+
+    static void Main(string[] args)
+    {        
+        string[] lines = File.ReadAllLines("../../../Advent/2023_day13.txt");         
+        int NbrOfRows = lines.Length;
+        int NbrOfColumns = lines[0].Length;
+
+        List<myPattern> Patterns = new();
+        bool newPattern = true;
+        int ListIndex = 0;
+        int startIndex = 1;
+        //FindRowValues
+        for(int i= 0; i < lines.Length; i++){
+            if(lines[i]==""){
+                newPattern = true;
+                Patterns.ElementAt(ListIndex).nbrOfRows = i-startIndex;
+                ListIndex++;
+                continue;
+            }            
+            if(newPattern){
+                startIndex = i;
+                Patterns.Add(new myPattern(columns:lines[i].Length,startind:startIndex));                
+                newPattern = false;
+            }
+
+            long rowValue = 0;
+
+            for(int j=0; j < lines[i].Length; j++){
+                rowValue += (lines[i][j].Equals('#') ? 1 : 0) << j;
+            }
+            Patterns.ElementAt(ListIndex).RowList.Add(rowValue);
+        }
+
+        //for last one:
+        Patterns.ElementAt(ListIndex).nbrOfRows = lines.Length-startIndex;
+        ListIndex++;
+
+        //find Column values
+        for(int i = 0; i <Patterns.Count; i++){
+            for(int colindex = 0; colindex<Patterns.ElementAt(i).nbrOfColumns; colindex++){
+                long columnValue = 0;
+                for(int j = 0 ; j<Patterns.ElementAt(i).nbrOfRows; j++){
+                    columnValue += (lines[Patterns.ElementAt(i).startIndex+j][colindex].Equals('#') ? 1 : 0) << j;
+                    
+                }
+                Patterns.ElementAt(i).ColumnList.Add(columnValue);
+            }
+            
+        }
+
+        long countTotal = 0;
+        foreach(var pattern in Patterns){
+            countTotal += findMirror(pattern.ColumnList,pattern.nbrOfColumns) + 100*findMirror(pattern.RowList,pattern.nbrOfRows);     
+        }      
+        Console.WriteLine(countTotal);
+    }
+
+    
+}
---
+++ Advent2023_day17/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+//I made Dijkstras algorithm, but failed to add constraint, then i instead got inspired by another solution on the internet, but dont work yet..
+enum Direction{
+
+    DIRECTION_UNDEFINED,
+    DIRECTION_UP,
+    DIRECTION_DOWN,
+    DIRECTION_LEFT,
+    DIRECTION_RIGHT
+  };
+
+class Position{
+
+
+    public int row = 0;
+
+    public int column = 0;
+
+    public Position(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+}
+class Node{
+
+    public Node(Direction dir, Position pos, int steps, int distance){
+        this.currentDirection = dir;
+        this.nodePosition = pos;
+        this.stepsOneDir = steps;
+        this.distanceToStart = distance;
+    }
+
+    public bool visited = false;
+
+    public int distanceToStart = int.MaxValue;
+
+    public Position nodePosition = new(0,0);
+
+    public int Row{
+        get {return nodePosition.row;}
+        set { nodePosition.row = value;}
+    }
+
+    public int Col{
+        get {return nodePosition.column;}
+        set { nodePosition.column = value;}
+    }
+
+    public Direction currentDirection = Direction.DIRECTION_UNDEFINED;
+
+    public int stepsOneDir = 0;
+
+
+}
+
+
+
+class DAY17
+{
+    public static bool PointInGrid(Position startpoint, bool ccw, Direction dir, int[,] grid, out Node newNode, int steps, bool forward){
+        newNode = new Node(Direction.DIRECTION_UNDEFINED,new Position(startpoint.row,startpoint.column),steps,0);
+
+        bool inGrid = false;
+
+        switch(dir){
+
+            case Direction.DIRECTION_UP:     
+            if(forward){
+                newNode.Row--;
+            }   
+            else{
+                newNode.Col += (ccw ? -1 : 1);
+                newNode.currentDirection = ccw ? Direction.DIRECTION_LEFT : Direction.DIRECTION_RIGHT;                
+            }        
+                
+                break;
+
+            case Direction.DIRECTION_DOWN:
+            if(forward){
+                newNode.Row++;
+            }   
+            else{
+                newNode.Col += (ccw ? 1 : -1);
+                newNode.currentDirection = ccw ? Direction.DIRECTION_RIGHT : Direction.DIRECTION_LEFT;
+            }
+                break;
+
+            case Direction.DIRECTION_LEFT:
+            if(forward){
+                newNode.Col--;
+            }   
+            else{
+                newNode.Row += (ccw ? 1 : -1);
+                newNode.currentDirection = ccw ? Direction.DIRECTION_DOWN : Direction.DIRECTION_UP;
+            }
+                break;
+
+            case Direction.DIRECTION_RIGHT:
+            if(forward){
+                newNode.Col++;
+            }   
+            else{
+                newNode.Row += (ccw ? -1 : 1);
+                newNode.currentDirection = ccw ? Direction.DIRECTION_UP : Direction.DIRECTION_DOWN;
+            }
+                break;
+
+        }
+        inGrid = (newNode.Col >= 0 && newNode.Col < grid.GetLength(1)) && (newNode.Row >= 0 && newNode.Row < grid.GetLength(0));
+
+        if(inGrid){
+            Console.WriteLine("row: "+ newNode.Row +" Col: "+ newNode.Col);
+            newNode.distanceToStart = grid[newNode.Row,newNode.Col];  
+        }
+        return inGrid;
+    }
+    static void Main(string[] args)
+    {        
+        string[] lines = File.ReadAllLines("../../../Advent/2023_day17.txt");         
+        int NbrOfRows = lines.Length;
+        int NbrOfColumns = lines[0].Length;
+
+        int[,] heatMap = new int[NbrOfRows,NbrOfColumns];
+
+        PriorityQueue<Node,int> nodeQueue = new PriorityQueue<Node,int>(); 
+
+
+        for(int i = 0; i< NbrOfRows; i++){
+            for(int j = 0; j< NbrOfColumns; j++){
+                heatMap[i,j] = Convert.ToInt32(lines[i][j]-'0');
+            }
+        }
+
+        //https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+
+        int countCost = 0;
+        nodeQueue.Enqueue(new Node(Direction.DIRECTION_DOWN,new Position(0,0),0,0),0);
+        nodeQueue.Enqueue(new Node(Direction.DIRECTION_RIGHT,new Position(0,0),0,0),0);
+        Console.WriteLine("nodecount:  " + nodeQueue.Count);
+    
+        HashSet<(int,int, int)> visitedNodes = new HashSet<(int,int,int)>();
+        
+        while(nodeQueue.Count != 0){
+            Console.WriteLine("nodecount:  " + nodeQueue.Count);
+            Node currentNode = nodeQueue.Dequeue();
+            if(currentNode.Row == NbrOfRows-1 && currentNode.Col == NbrOfColumns -1){
+                countCost = currentNode.distanceToStart;
+                break;
+            }
+
+            if(visitedNodes.Contains((currentNode.nodePosition.row,currentNode.nodePosition.column,currentNode.stepsOneDir))){
+                continue;
+            }
+            //Console.WriteLine("Node:  "+ currentNode.nodePosition.row + ", " + currentNode.nodePosition.column + " steps: " + currentNode.stepsOneDir);
+            visitedNodes.Add((currentNode.nodePosition.row,currentNode.nodePosition.column,currentNode.stepsOneDir));
+            
+            //if CCW add in queue
+            if(PointInGrid(currentNode.nodePosition,true,currentNode.currentDirection,heatMap, out Node newNode,1,false)){
+                newNode.distanceToStart += currentNode.distanceToStart;
+                nodeQueue.Enqueue(newNode,newNode.distanceToStart);
+            }                
+
+            //if CW add in queue
+            if(PointInGrid(currentNode.nodePosition,false,currentNode.currentDirection,heatMap, out newNode,1,false)){
+                newNode.distanceToStart += currentNode.distanceToStart;
+                nodeQueue.Enqueue(newNode,newNode.distanceToStart);
+            }
+
+            if(currentNode.stepsOneDir < 3 && PointInGrid(currentNode.nodePosition,false,currentNode.currentDirection,heatMap, out newNode,currentNode.stepsOneDir + 1,true))
+            {
+                newNode.distanceToStart += currentNode.distanceToStart;
+                nodeQueue.Enqueue(newNode,newNode.distanceToStart);
+            }      
+            
+        }
+
+        Console.WriteLine("countCost: " + countCost);
+    }
+}
