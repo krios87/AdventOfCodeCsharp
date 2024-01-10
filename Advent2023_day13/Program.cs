d9dbeed3b836cb4b463630dc7ff4173d2fb31209
Kristofer Rosquist <kristofer.rosquist@gmail.com>
2024-01-10 17:00:51 +0100

    added day 13

--- Advent2023_day13/Program.cs
+++ Advent2023_day13/Program.cs
@@ -1,13 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Drawing;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-
 
 class myPattern{
     public int startIndex = 0;
@@ -19,47 +10,65 @@ class myPattern{
         nbrOfColumns = columns;
         startIndex = startind;
     }
+    public int reflectionLineRow = 0;
+    public int reflectionLineColumn = 0;
 }
 
 class DAY13
 {
-
-    static int findMirror(List<long> RowOrColumnList, int NbrOfRowOrColumn){
+    static int findMirror(List<long> RowOrColumnList, int NbrOfRowOrColumn, int previousvalue){
+        bool Found = false;
         for(int i = 0; i < NbrOfRowOrColumn; i++ ){
-                int previousI = i - 1;
-                bool Found = false;
-                    if(previousI >= 0 && RowOrColumnList.ElementAt(i) == RowOrColumnList.ElementAt(previousI)){
-                        int nextI = i + 1;
-                        Found = true;
-                        while(previousI > 0 && nextI < NbrOfRowOrColumn){
-                            previousI--;
-                            Found = Found && (RowOrColumnList.ElementAt(nextI) == RowOrColumnList.ElementAt(previousI));
-                            nextI++;
-                            if(!Found){
-                                break;
-                            }
-                        }                        
+            int previousI = i - 1;
+            if(previousI >= 0 && RowOrColumnList.ElementAt(i) == RowOrColumnList.ElementAt(previousI)){
+                int nextI = i + 1;
+                Found = true;
+                while(previousI > 0 && nextI < NbrOfRowOrColumn){
+                    previousI--;
+                    Found = Found && (RowOrColumnList.ElementAt(nextI) == RowOrColumnList.ElementAt(previousI));
+                    nextI++;
+                    if(!Found){
+                        break;
                     }
-                    if(Found){
-                        return i;
-                    }               
+                }                        
             }
+            if(Found){
+                if(previousvalue == 0 || i != previousvalue)
+                {
+                    return i;
+                }
+                else{
+                    Found = false;
+                }
+                
+            }               
+        }            
+        return 0;
+    }
 
+    static int FindMirrowPart2(List<long> RowOrColumnList, int NbrOfRowOrColumn, int NbrOfBitShifts, int previousvalue){
+            for(int i= 0; i < NbrOfRowOrColumn; i++){
+                for(int k = 0; k < NbrOfBitShifts; k++){                    
+                    RowOrColumnList[i] = RowOrColumnList[i] ^ (0x1 << k);
+                    int valueRoworColumn = findMirror(RowOrColumnList,NbrOfRowOrColumn,previousvalue);                    
+                    RowOrColumnList[i] = RowOrColumnList[i] ^ (0x1 << k);
+                    if(valueRoworColumn != 0){
+                        return valueRoworColumn;
+                    }                    
+                }           
+            }
             return 0;
-
     }
 
     static void Main(string[] args)
     {        
         string[] lines = File.ReadAllLines("../../../Advent/2023_day13.txt");         
-        int NbrOfRows = lines.Length;
-        int NbrOfColumns = lines[0].Length;
 
         List<myPattern> Patterns = new();
         bool newPattern = true;
         int ListIndex = 0;
         int startIndex = 1;
-        //FindRowValues
+        //add row values (represent # as 1 and . as 0 in binary for one value per row)
         for(int i= 0; i < lines.Length; i++){
             if(lines[i]==""){
                 newPattern = true;
@@ -74,7 +83,6 @@ class DAY13
             }
 
             long rowValue = 0;
-
             for(int j=0; j < lines[i].Length; j++){
                 rowValue += (lines[i][j].Equals('#') ? 1 : 0) << j;
             }
@@ -85,24 +93,33 @@ class DAY13
         Patterns.ElementAt(ListIndex).nbrOfRows = lines.Length-startIndex;
         ListIndex++;
 
-        //find Column values
+        //add Column values
         for(int i = 0; i <Patterns.Count; i++){
             for(int colindex = 0; colindex<Patterns.ElementAt(i).nbrOfColumns; colindex++){
                 long columnValue = 0;
                 for(int j = 0 ; j<Patterns.ElementAt(i).nbrOfRows; j++){
-                    columnValue += (lines[Patterns.ElementAt(i).startIndex+j][colindex].Equals('#') ? 1 : 0) << j;
-                    
+                    columnValue += (lines[Patterns.ElementAt(i).startIndex+j][colindex].Equals('#') ? 1 : 0) << j;                    
                 }
                 Patterns.ElementAt(i).ColumnList.Add(columnValue);
-            }
-            
+            }            
         }
 
-        long countTotal = 0;
+        int countTotal = 0;
         foreach(var pattern in Patterns){
-            countTotal += findMirror(pattern.ColumnList,pattern.nbrOfColumns) + 100*findMirror(pattern.RowList,pattern.nbrOfRows);     
+            pattern.reflectionLineColumn = findMirror(pattern.ColumnList,pattern.nbrOfColumns,0);
+            pattern.reflectionLineRow = findMirror(pattern.RowList,pattern.nbrOfRows,0);
+            countTotal += pattern.reflectionLineColumn + 100*pattern.reflectionLineRow;     
         }      
-        Console.WriteLine(countTotal);
+        Console.WriteLine("part 1: " + countTotal);
+
+        //part2:
+        countTotal = 0;
+        foreach(var pattern in Patterns){
+            countTotal += 100*FindMirrowPart2(pattern.RowList, pattern.nbrOfRows, pattern.nbrOfColumns, pattern.reflectionLineRow) +
+                          FindMirrowPart2(pattern.ColumnList, pattern.nbrOfColumns, pattern.nbrOfRows, pattern.reflectionLineColumn);
+        }      
+        
+        Console.WriteLine("part 2: " + countTotal);
     }
 
     
