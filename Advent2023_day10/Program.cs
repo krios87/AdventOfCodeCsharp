7aade0ba9408eea19717d1a0a39f0d2f3b4596b0
Kristofer Rosquist <kristofer.rosquist@gmail.com>
2024-01-13 21:04:28 +0100

    add day 10

---
+++ Advent2023_day10/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+
+enum directionMove{
+    DIRECTION_UP,
+    DIRECTION_DOWN,
+    DIRECTION_LEFT,
+    DIRECTION_RIGHT
+} ;
+
+class DAY10
+{
+
+    public static directionMove getNewDirection(directionMove direction,char currentChar){
+        return direction switch
+        {
+            directionMove.DIRECTION_UP => currentChar == '7' ? directionMove.DIRECTION_LEFT : currentChar == 'F' ? directionMove.DIRECTION_RIGHT : directionMove.DIRECTION_UP,
+            directionMove.DIRECTION_DOWN => currentChar == 'J' ? directionMove.DIRECTION_LEFT : currentChar == 'L' ? directionMove.DIRECTION_RIGHT : directionMove.DIRECTION_DOWN,
+            directionMove.DIRECTION_LEFT => currentChar == 'F' ? directionMove.DIRECTION_DOWN : currentChar == 'L' ? directionMove.DIRECTION_UP : directionMove.DIRECTION_LEFT,
+            directionMove.DIRECTION_RIGHT => currentChar == '7' ? directionMove.DIRECTION_DOWN : currentChar == 'J' ? directionMove.DIRECTION_UP : directionMove.DIRECTION_RIGHT,
+            _ => directionMove.DIRECTION_UP,
+        };
+    }
+
+    public static (int,int) getNewCoord((int,int) coord, directionMove direction){
+        return direction switch{
+            directionMove.DIRECTION_UP => (coord.Item1,coord.Item2+1),
+            directionMove.DIRECTION_DOWN => (coord.Item1,coord.Item2-1),
+            directionMove.DIRECTION_LEFT => (coord.Item1-1,coord.Item2),
+            directionMove.DIRECTION_RIGHT => (coord.Item1+1,coord.Item2),
+            _ => (coord.Item1,coord.Item2),
+        };
+    }
+
+    public static int countSteps(List<(int,int)> points,char[,] map, int startX, int startY, directionMove startDirection){
+        int count = 1;
+        points.Add(new (startX,startY)); //add start coord
+        (int currentX,int currentY) = getNewCoord((startX,startY),startDirection);
+        directionMove currentDirection = getNewDirection(startDirection,map[currentX,currentY]);
+        while(!(currentX == startX && currentY == startY)){
+            count++;
+            (currentX,currentY) = getNewCoord((currentX,currentY),currentDirection);            
+            currentDirection = getNewDirection(currentDirection,map[currentX,currentY]);
+            points.Add(new (currentX,currentY)); //add point for part 2
+        }
+
+        return count;
+
+    }
+    //https://en.wikipedia.org/wiki/Shoelace_formula
+    public static long calcAreaSholaceFormula(List<(int,int)> points){
+
+        long area = 0;
+        for(int i = 0; i < points.Count; i++){
+            area += points[i].Item1*points[(i+1)%points.Count].Item2-points[(i+1)%points.Count].Item1*points[i].Item2;
+        }
+        
+        return Math.Abs(area/2);
+    }
+    static void Main(string[] args)
+    {        
+        string[] lines = File.ReadAllLines("../../../Advent/2023_day10.txt");  
+        int nbrOfXcoord = lines[0].Length;
+        int nbrOfYcoord = lines.Length;
+        char[,] map = new char[nbrOfXcoord,nbrOfYcoord];
+        int startX = 0;
+        int startY = 0;
+
+        for(int i=0; i< nbrOfYcoord; i++){
+            for(int j=0; j< nbrOfXcoord; j++){
+                int Ycoord = nbrOfYcoord-i-1;////make sure coordinate 0,0 is in bottom left so invert Y since row is read from above
+                map[j,Ycoord] = lines[i].ElementAt(j); 
+                if(map[j,Ycoord] == 'S'){
+                    startX = j;
+                    startY = Ycoord;
+                }
+            }
+        }      
+        List<(int,int)> points = new(); //points for part 2
+        //start stepping up
+        int count = countSteps(points, map, startX, startY, directionMove.DIRECTION_UP);        
+        Console.WriteLine("count:" + count/2); //half steps around is the solution
+
+        //part2:
+        long area = calcAreaSholaceFormula(points);
+        //picks theroem (https://en.wikipedia.org/wiki/Pick's_theorem) i = A - b/2 +1 (this calculates interior points)
+        Console.WriteLine("count part 2: " + (area - count/2 +1));
+        
+    }
+}
--- Advent2023_day18/Program.cs
+++ Advent2023_day18/Program.cs
@@ -14,45 +14,17 @@ class DAY18
 
         long area = 0;
         for(int i = 0; i < points.Count; i++){
-            
-            if(i==(points.Count -1)){
-                area += points[i].Item1*points[0].Item2-points[0].Item1*points[i].Item2; // determinants
-            }
-            else{
-                area += points[i].Item1*points[(i+1)].Item2-points[(i+1)].Item1*points[i].Item2; // determinants
-            }
-
-        //Console.WriteLine("Point: " + points[i].Item1 + "," + points[i].Item2 + " area: " + area);
+            area += points[i].Item1*points[(i+1)%points.Count].Item2-points[(i+1)%points.Count].Item1*points[i].Item2;
         }
         
         return Math.Abs(area/2);
     }
     public static void calculateCoordinateStep1(List<(long,long)> points,string direction,ref int currentX,ref int currentY, ref long exteriorLength, long length){
-        
-
         for(long i = 0; i< length; i ++){
             exteriorLength ++;
-
             points.Add(new (currentX,currentY));
-        
-            switch(direction){
-
-                case "R":
-                    currentX++;
-                    break;
-
-                case "L":
-                    currentX--;
-                    break;
-
-                case "U":
-                    currentY++;
-                    break;
-
-                case "D":
-                    currentY--;
-                    break;
-            }
+            currentX += direction is "R" or "0" ? 1 : direction is "L" or "2" ? -1 : 0;
+            currentY += direction is "U" or "3" ? 1 : direction is "D" or "1" ? -1 : 0;
         }
     }
 
@@ -70,44 +42,30 @@ class DAY18
             linesSplit[i] = lines[i].Split(' ',StringSplitOptions.RemoveEmptyEntries);
         }      
         
+        //part 1:
         List<(long,long)> Points = new();
-
-        long exteriorLength = 0;
+        long boundrary = 0;
         for(int i = 0; i< lines.Length; i++){
-            calculateCoordinateStep1(Points,linesSplit[i][0],ref currentX,ref currentY, ref exteriorLength, Convert.ToInt64(linesSplit[i][1]));
+            calculateCoordinateStep1(Points,linesSplit[i][0],ref currentX,ref currentY, ref boundrary, Convert.ToInt64(linesSplit[i][1]));
         }
         long area = calcAreaSholaceFormula(Points);
 
         //picks theroem (https://en.wikipedia.org/wiki/Pick's_theorem)
         //to calculate i = A - b/2 +1 (this calculates interior points, but we want to include border, b, so we add b) => i + b = A + b/2 +1 
-        Console.WriteLine("count part 1: " + (exteriorLength/2 + area +1) + " area: " + area + " edge: " + exteriorLength);
+        Console.WriteLine("count part 1: " + (boundrary/2 + area +1));
 
         //part 2:
         List<(long,long)> Points2 = new();
-        exteriorLength = 0;
+        boundrary = 0;
         currentX = initialX;
         currentY = initialY;
         for(int i = 0; i< lines.Length; i++){
             int.TryParse(linesSplit[i][2].Substring(2,5), System.Globalization.NumberStyles.HexNumber, null, out int length); //take 5 digits for length
-            string direction = linesSplit[i][2].Substring(7,1) switch //last digit is direction
-            {
-                string s when s == "0" => "R",
-                string s when s == "1" => "D",
-                string s when s == "2" => "L",
-                string s when s == "3" => "U",
-                _ => "E"
-            };
-            calculateCoordinateStep1(Points2,direction,ref currentX,ref currentY, ref exteriorLength, length);
+            calculateCoordinateStep1(Points2,linesSplit[i][2].Substring(7,1),ref currentX,ref currentY, ref boundrary, length); //last digit is direction
         }
 
         area = calcAreaSholaceFormula(Points2);
-        
 
-
-        
-
-
-        Console.WriteLine("count part 2: " + (exteriorLength/2 + area +1) + " area: " + area + " edge: " + exteriorLength);
-
+        Console.WriteLine("count part 2: " + (boundrary/2 + area +1));
     }
 }
