20487ac633cccfae736e9ad65729680cd6d3064d
Kristofer Rosquist <kristofer.rosquist@gmail.com>
2024-01-14 12:07:22 +0100

    changed to a list from dictionary

--- Advent2023_day8/Program.cs
+++ Advent2023_day8/Program.cs
@@ -1,29 +1,14 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Drawing;
-using System.Runtime.CompilerServices;
-
-
 
 class MyNode{
-
-    public string name = "";
-
-    public MyNode? RightNode;
-
-    public MyNode? LeftNode;
-
-    public MyNode(string name){
-        this.name = name;
-    }
+    public string ?Name { get; set; }
+    public MyNode ?RightNode { get; set; }
+    public MyNode ?LeftNode { get; set; }
+    public MyNode(string name) => Name = name;
 }
-
 
 class DAY8
 {
-
     public static MyNode GetNode(char leftorRight, MyNode currentNode){
         return leftorRight == 'L' ? currentNode.LeftNode : currentNode.RightNode;
     }
@@ -33,33 +18,33 @@ class DAY8
         string[] lines = File.ReadAllLines("../../../Advent/2023_day8.txt");  
 
         //create Nodes without pointer to other nodes
-        Dictionary<string,MyNode> allNodesDict = new();
+        List<MyNode> allNodesList = new();
         int startIndex = 0;
         List<MyNode> ghostNodes = new();
         for(int i=2; i< lines.Length; i++){
-            allNodesDict.Add(lines[i][..3], new MyNode(lines[i][..3])); //do regex instead to find nodes
+            allNodesList.Add(new MyNode(lines[i][..3])); //do regex instead to find nodes
             if(lines[i][..3].Contains("AAA")){
                 startIndex = i-2;
             }
             if(lines[i][2] == 'A'){
-                ghostNodes.Add(allNodesDict.Last().Value);
+                ghostNodes.Add(allNodesList.Last());
             }
         }      
 
         //add pointers to Right and left nodes
-        for(int i=0; i<allNodesDict.Count;i++){
+        for(int i=0; i<allNodesList.Count;i++){
             string[] TwoNodes = lines[i+2].Substring(lines[i+2].IndexOf('(') + 1,8).Split(',',StringSplitOptions.TrimEntries); 
             string leftNode = TwoNodes[0];
             string rightNode = TwoNodes[1];
-            allNodesDict.ElementAt(i).Value.LeftNode = allNodesDict[leftNode];
-            allNodesDict.ElementAt(i).Value.RightNode = allNodesDict[rightNode];            
-        }
-        
+            allNodesList.ElementAt(i).LeftNode = allNodesList.ElementAt(allNodesList.FindIndex(x => x.Name == leftNode));
+            allNodesList.ElementAt(i).RightNode = allNodesList.ElementAt(allNodesList.FindIndex(x => x.Name == rightNode));            
+        }        
 
-        MyNode currentNode = allNodesDict.ElementAt(startIndex).Value;
+        //part 1
+        MyNode currentNode = allNodesList.ElementAt(startIndex);
         int indexNode = 0;
         int steps = 0; 
-        while(currentNode.name != "ZZZ"){
+        while(currentNode.Name != "ZZZ"){
             currentNode = GetNode(lines[0][indexNode%lines[0].Length],currentNode);
             indexNode++;
             steps++;
@@ -69,15 +54,14 @@ class DAY8
 
         //part 2
         indexNode = 0;
-        List<int> stepsList = new();
-        
+        List<int> stepsList = new();        
         //calculate for each how many steps to an end, assuming it will be a repeating cycle we can calculate least common multiple for all ghosts (LCM)
         for(int i = 0 ; i< ghostNodes.Count; i++){
             bool foundEnd = false;
             steps = 0;
             while(!foundEnd){                    
                 ghostNodes[i] = GetNode(lines[0][indexNode%lines[0].Length],ghostNodes[i]);
-                foundEnd = ghostNodes[i].name[2] == 'Z';
+                foundEnd = ghostNodes[i].Name[2] == 'Z';
                 steps++;
                 indexNode++;
             }
